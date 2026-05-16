fcc19cc36ab0d40361b13103e4427428f30d989e
MesS00r <art65734521@gmail.com>
2026-05-16 21:52:41 +0300

    added vga color lib (nim)

--- src/bootloader/boot.asm
+++ src/bootloader/boot.asm
@@ -8,55 +8,56 @@ _start:
 ; $=======================================$
 ; | STANDARD INIT                         |
 ; $=======================================$
-    xor ax, ax
-    mov ds, ax
-    mov es, ax
+    xor ax, ax ; очистить ax
+    mov ds, ax ; очистить ds
+    mov es, ax ; очистить es
 
-    mov [boot_drive], dl
+    mov [boot_drive], dl ; сохранить dl в boot_drive
 
-    cli
-    mov ss, ax
-    mov sp, 0x7C00
-    sti
+    cli            ; запретить прерывания
+    mov ss, ax     ; очистить ss
+    mov sp, 0x7C00 ; установить стек на 0x7C00
+    sti            ; разрешить прерывания
 
-    mov si, ax
-    mov di, ax
-    cld
+    mov si, ax ; очистить si
+    mov di, ax ; очистить di
+    cld        ; сброс DF (флага направления)
 
 ; $=======================================$
 ; | MAIN CODE                             |
 ; $=======================================$
-    mov ah, 0x02
-    mov al, 8
-    mov ch, 0
-    mov cl, 2
-    mov dh, 0
-    mov dl, [boot_drive]
-    mov bx, KERNEL_MAIN
-    int 0x13
-    jc disk_err
+    mov ah, 0x02         ; 0x02 - функция чтения с диска
+    mov al, 8            ; 8 секторов для чтения
+    mov ch, 0            ; 0 - номер цилиндра
+    mov cl, 2            ; 2 - номер сектора
+    mov dh, 0            ; 0 - номер головки
+    mov dl, [boot_drive] ; загружаем сохр. dl (boot_drive) (номер диска)
+    mov bx, KERNEL_MAIN  ; записываем адрес буфера памяти в bx
+    int 0x13             ; BIOS прерывание 0x13
+    jc disk_err          ; переходим в disk_err, если произошла ошибка
 
-    mov ah, 0
-    mov al, 0x13
-    int 0x10
+    mov ah, 0x00 ; 0x00 - функция "установить видеорежим"
+    mov al, 0x13 ; 0x13 - установить режим: 320x200, 256 color, VGA
+    int 0x10     ; BIOS прерывание 0x10
 
-    cli
+    cli ; запретить прерывания
 
 ; $=======================================$
 ; | TRANSITION TO PROTECTED MODE          |
 ; $=======================================$
-    lgdt [gdt]
+    lgdt [gdt] ; загрузить таблицу дескрипторов
 
-    mov eax, cr0
-    or eax, 1
-    mov cr0, eax
+    mov eax, cr0 ; загрузить cr0 в eax
+    or eax, 1    ; установить 0-й бит на 1
+    mov cr0, eax ; загрузить eax в cr0
 
-    jmp 0x08:_32bit_start
+    jmp 0x08:_32bit_start ; дальний переход в _32bit_start
 
 ; $=======================================$
 ; | GDT                                   |
 ; $=======================================$
-align 4
+align 4 ; выравнивание на 4
+; далее идут таблицы дескрипторов
 gdt_start:
     dd 0, 0
 gtb_code:
@@ -75,18 +76,18 @@ gdt:
 ; | DISK ERROR                            |
 ; $=======================================$
 disk_err:
-    mov si, err_msg
+    mov si, err_msg ; загрузить адрес err_msg в si
 .loop:
-    lodsb
-    test al, al
-    jz .done
-    mov ah, 0x0E
-    mov bh, 0
-    int 0x10
-    jmp .loop
+    lodsb           ; загрузить символ из si (err_msg) в al
+    test al, al     ; проверить al
+    jz .done        ; переход в .done, если ZF = 1
+    mov ah, 0x0E    ; 0x0E - функция печати символа без атрибутов
+    mov bh, 0       ; 0 - номер страницы
+    int 0x10        ; BIOS прерывание 0x10
+    jmp .loop       ; переход в .loop (цикл)
 .done:
-    hlt
-    jmp $
+    hlt             ; остановить процессор
+    jmp $           ; бесконечный цикл, если hlt не сработал
 
 err_msg: db "Disk read error.", 10, 13, 0
 boot_drive: db 0
@@ -96,14 +97,15 @@ boot_drive: db 0
 ; $=======================================$
 [BITS 32]
 _32bit_start:
-    mov ax, 0x10
-    mov ds, ax
-    mov es, ax
-    mov fs, ax
-    mov gs, ax
-    mov ss, ax
-    mov esp, 0x7C00
+    mov ax, 0x10    ; загрузить 0x10 в ax
+    mov ds, ax      ; загрузить ax в ds
+    mov es, ax      ; загрузить ax в es
+    mov fs, ax      ; загрузить ax в fs
+    mov gs, ax      ; загрузить ax в gs
+    mov ss, ax      ; загрузить ax в ss
+    mov esp, 0x7C00 ; установить стек на 0x7C00
 
-    jmp KERNEL_MAIN
+    jmp KERNEL_MAIN ; переход в KERNEL_MAIN (0x7E00)
+    ; запуск ядра ОС (В 1 конец)
 
 KERNEL_MAIN equ 0x7E00
--- src/kernel.c
+++ src/kernel.c
@@ -1,15 +1,16 @@
 #include <stdint.h>
+#include <my_libs/vga_colors.h>
+
+#define SCREEN_WIDTH 320
+#define SCREEN_HEIGHT 200
 
 __attribute__((section(".main")))
 void _main(void) {
     uint8_t *vga = (uint8_t *)0xA0000;
 
     for (uint32_t i = 0; i < 10000; i++) {
-        vga[i] = 4;
+        vga[i] = RED;
     }
 
-    __asm__(
-        "hlt\n"
-        "jmp ."
-    );
+    __asm__("hlt\njmp .");
 }
