9ec85c87e229db91a2b1e0e2d7203e43a1d9ebb0
MesS00r <art65734521@gmail.com>
2026-08-02 23:10:08 +0300

    added demo main

--- src/kernel.c
+++ src/kernel.c
@@ -5,13 +5,23 @@
 
 __attribute__((section(".main")))
 void _main(void) {
+    uint8_t count  = 0;
+    uint8_t count1 = 0;
+
     while (1) {
         wait_frame();
         clear_screen();
     
-        tetromino_draw(10, 10, tetrominoes[3]);
+        count1 = (count / 15) % 7;
+        tetromino_draw(MAP_WIDTH  / 2 - 2,
+                       MAP_HEIGHT / 2 - 2,
+                       tetrominoes[count1]
+                      );
 
         VGA[0][0] = TILE;
+
+        if (count >= 255)count = 0;
+        else count++;
     }
     __asm__ volatile("hlt\njmp .");
 }
